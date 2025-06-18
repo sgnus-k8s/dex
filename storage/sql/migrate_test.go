607151109b42c1d16ec4f209a12bfcd9b4d4334a
yungsg <ysg@nus.edu.sg>
2025-06-18 21:53:10 +0800

    added patched files

---
+++ storage/sql/migrate_test.go
@@ -0,0 +1,48 @@
+//go:build cgo
+// +build cgo
+
+package sql
+
+import (
+	"database/sql"
+	"log/slog"
+	"testing"
+
+	sqlite3 "modernc.org/sqlite"
+)
+
+func TestMigrate(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	logger := slog.New(slog.DiscardHandler)
+
+	errCheck := func(err error) bool {
+		sqlErr, ok := err.(sqlite3.Error)
+		if !ok {
+			return false
+		}
+		return sqlErr.ExtendedCode == sqlite3.ErrConstraintUnique
+	}
+
+	var sqliteMigrations []migration
+	for _, m := range migrations {
+		if m.flavor == nil || m.flavor == &flavorSQLite3 {
+			sqliteMigrations = append(sqliteMigrations, m)
+		}
+	}
+
+	c := &conn{db, &flavorSQLite3, logger, errCheck}
+	for _, want := range []int{len(sqliteMigrations), 0} {
+		got, err := c.migrate()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != want {
+			t.Errorf("expected %d migrations, got %d", want, got)
+		}
+	}
+}
